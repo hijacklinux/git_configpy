9eb2fa7095fb6f6777e92006c23c4fbb209b851b
hijacklinux <handjane@163.com>
2017-08-21 05:00:20 +0800

    去掉一个是字

--- git_configpy.py
+++ git_configpy.py
@@ -4,7 +4,7 @@ import subprocess
 subprocess.Popen("git config --global user.name '你的github用户名'", shell=True)
 subprocess.Popen("git config --global user.email 你的邮箱", shell=True)
 subprocess.Popen("git config --global color.ui auto", shell=True)
-subprocess.Popen("git config --global core.editor vim", shell=True)  #你想要的编辑器，我是用vim
+subprocess.Popen("git config --global core.editor vim", shell=True)  #你想要的编辑器，我用vim
 subprocess.Popen("git config --global merge.tool vimdiff", shell=True) #设置你的合并工具
 ls=subprocess.Popen("ls ~/.ssh/", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True,shell=True)
 ls_out=ls.stdout.read()
